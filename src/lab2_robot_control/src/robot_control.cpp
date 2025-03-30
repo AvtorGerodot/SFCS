1260e0b2e10217475052578265450d31c2676c8b
Flrxx <e.a.yakovlev011@mali.ru>
2025-03-30 10:41:52 -0700

    some fixes

--- src/lab2_robot_control/src/robot_control.cpp
+++ src/lab2_robot_control/src/robot_control.cpp
@@ -1,15 +1,15 @@
 #include "rclcpp/rclcpp.hpp"        //Основной заголовочный файл ROS2 для C++, содержащий основные классы и функции для работы с нодами
   //Заголовочный файл для сообщения типа String из стандартного пакета std_msgs
 #include <chrono>
-#include "geometry_msgs/msg/Twist.msg"
-#include "sensor_msgs/msg/LaserScan.msg"
-#include "nav_msgs/msg/Odometry.msg"
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 class ControlNode : public rclcpp::Node {  //Определение класса ListenerNode, который наследуется от rclcpp::Node
 public:
     ControlNode() : Node("control_node") {  //Конструктор класса. Инициализирует ноду с именем "listener_node"
     //cоздаём подписку на топик "chatter" и функцией обработки callback
-    odomSubscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    subscription_  = this->create_subscription<sensor_msgs::msg::LaserScan>(
       "/base_scan", 10, std::bind(&ControlNode::laserCallback, this, std::placeholders::_1));
 
     //создаём издателя-ответчика, публикующего сообщения типа String в топик "reply"
@@ -30,8 +30,8 @@ private:
 //   }
 
   void laserCallback(sensor_msgs::msg::LaserScan msg){
-    const double kMinRange = 0.3;
-    obstacle = false
+    const double kMinRange = 0.5;
+    obstacle = false;
     for (size_t i = 0; i<msg.ranges.size(); i++)  //проверим нет ли вблизи робота препятствия
     {
         if (msg.ranges[i] < kMinRange)
@@ -45,10 +45,24 @@ private:
 
   void timerCallback(){
     geometry_msgs::Twist cmd;
-    cmd.linear.x = 0,5;
+    if (!obstacle)
+    {
+      cmd.linear.x = 0.5;
+      cmd.angular.z = 0;
+      }   
+    else
+      {
+      cmd.linear.x = 0;
+      cmd.angular.z = 0.5;
+    }
     publisher_->publish(cmd);
   }
 
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 };
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ControlNode>());
+  rclcpp::shutdown();
+  return 0;
+}
