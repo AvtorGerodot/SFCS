66aa26ee268b0626c2750c506518683f19eefed7
Flrxx <e.a.yakovlev011@mali.ru>
2025-05-10 09:32:44 -0700

    added .h

--- src/lab5_simple_map/src/simple_map.cpp
+++ src/lab5_simple_map/src/simple_map.cpp
@@ -1,13 +1,4 @@
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/laser_scan.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include "tf2/transform_datatypes.h"
-#include "tf2_ros/buffer.h"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-
-using namespace std::chrono_literals;
+#include "simple_map.h"
 
 class SimpleMap : public rclcpp::Node
 {
---
+++ src/lab5_simple_map/src/simple_map.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2/transform_datatypes.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+
+using namespace std::chrono_literals;
